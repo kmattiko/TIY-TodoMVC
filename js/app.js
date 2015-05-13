617519d47519b7152035b2285f718e458f95b95b
Kim Mattiko <amberlie@rocketmail.com>
2015-05-13 16:20:24 -0400

    got whatever to console.log when keyup press

--- js/app.js
+++ js/app.js
@@ -4,3 +4,49 @@
 	// Your starting point. Enjoy the ride!
 
 })(window);
+
+//What needs to be done will allow typing but will not allow adding
+    //Something needs added for the enter key
+		//Will need to be pushed into the Array with the list.
+
+document.head.parentElement.className = "js";
+
+var addTodos = document.querySelector('header')
+
+addTodos.addEventListener('keydown', function(){
+	console.log("whatever");
+})
+
+// function (addTodos.addEventListener)
+// 	if (e.keyCode == 13) {
+// 		(this).appendTo('view');
+// 	};
+
+
+/*function(addTodos, index, addTodos) {
+	addTodos.addEventListener(e.keyCode == 13);
+	(this).appendTo('view');
+};
+
+/*(".like-button").click(function() {
+		 (this).toggle("active");
+		  });
+		};
+		(document).ready(addTodos);
+		var elements = document.querySelectorAll('h3');
+
+_.forEach(elements, function(element, index, elements){
+    element.addEventListener('click', function(){
+        console.log(elements, index, element);
+        element.parentElement.classList.toggle('cbp-ntopen');
+    });
+});
+
+function runScript(e) {
+    if (e.keyCode == 13) {
+        var tb = document.getElementById("scriptBox");
+        eval(tb.value);
+        return false;
+    }
+}
+*/
