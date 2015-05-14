11db1045950dd918b2f9291e2386c196667c485a
Kim Mattiko <amberlie@rocketmail.com>
2015-05-13 20:44:50 -0400

    attempting to get enter key press to work, adding attempts as memos below

--- js/app.js
+++ js/app.js
@@ -9,7 +9,39 @@
     //Something needs added for the enter key
 		//Will need to be pushed into the Array with the list.
 
-document.head.parentElement.className = "js";
+	document.head.parentElement.className = "js";
+
+	var addTodos = document.querySelector('header')
+
+	addTodos.addEventListener('keypress' == 13, function(){
+		console.log("whatever");
+	})
+
+/*
+
+			var KeyEvent = {
+				DOM_VK_RETURN: 13,
+			};
+		}
+
+		addTodos.addEventListener(KeyEvent(13), function(){
+			console.log("whatever");
+		})
+
+		/*
+		function (e) {
+		    var code = check.addEventListener;
+		    //Up arrow pressed
+		    if (code == 13) {
+		        console.log("You pressed the Up arrow key");
+					}
+		    else {
+		        console.log("You pressed some other key I don't really care about.");
+				}
+/*				http://stackoverflow.com/questions/12241113/whats-the-best-way-to-create-key-events-in-html5-canvas*/
+
+
+/*document.head.parentElement.className = "js";
 
 var addTodos = document.querySelector('header')
 
